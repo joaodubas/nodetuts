4682bf6fb4b930fbeac092ba0ecd05492dfebc6b
Joao P Dubas <joao.dubas@gmail.com>
2011-03-12 19:46:26 -0300

    Lesson 02 from node tuts: child_process and spawn

---
+++ node_02/node_02.js
@@ -0,0 +1,23 @@
+var http = require('http'),
+	spawn = require('child_process').spawn
+	;
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+	
+	var tail_child = spawn('tail',
+		['-f', '/var/log/syslog']
+	);
+	
+	req.connection.on('end', function () {
+		tail_child.kill();
+	});
+	
+	tail_child.stdout.on('data', function (data) {
+		console.log(data.toString());
+		res.write(data);
+	});
+	
+}).listen(4000);
