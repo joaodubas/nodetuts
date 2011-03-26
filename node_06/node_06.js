18af3a26790307aca5cd9ee7a6a1d3224b4d6f28
Joao P Dubas <joao.dubas@gmail.com>
2011-03-25 23:14:08 -0300

    Lesson 06 from node tuts: concluded chat with websockets

--- node_06/node_06.js
+++ node_06/node_06.js
@@ -1,6 +1,15 @@
 var http = require('http')
 	, fs = require('fs')
 	, util = require('util')
+	, ws = require('./src/ws/ws.js')
+	, clients = []
+	, broadcast = function (showAll, clientList, sender, message) {
+		clientList.forEach(function (client) {
+			if (showAll || client !== sender) {
+				client.write(message);
+			}
+		});
+	}
 	;
 
 http.createServer(function (req, res) {
@@ -16,3 +25,43 @@ http.createServer(function (req, res) {
 		}
 	});
 }).listen(4000);
+
+ws.createServer(function (websocket) {
+	var username = null;
+	
+	websocket.on('connect', function (resource) {
+		clients.push(websocket);
+		websocket.write('Fala seu pulha!!!!')
+		websocket.write('Não que eu me interesse, mas qual o teu nome?');
+	});
+	
+	websocket.on('data', function (data) {
+		var feedback = ''
+			, showAll = true
+			;
+
+		if (!username) {
+			username = data.toString();
+			websocket.write('Porque você está aqui ' + username + '?');
+			feedback = username + ' esta na sala!';
+			showAll = false;
+		} else {
+			feedback = username + ' falou: ' + data.toString();
+			showAll = true;
+		}
+		
+		broadcast(showAll, clients, websocket, feedback);
+	});
+	
+	websocket.on('close', function () {
+		var idx = clients.indexOf(websocket)
+			, feedback = username + ' abandonou vocês!';
+			;
+		
+		if (idx >= 0) {
+			clients.splice(idx, 1);
+		}
+		
+		broadcast(true, clients, websocket, feedback);
+	});
+}).listen(8080);
